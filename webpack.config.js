c4f59612f624280762b0ec6b63dd73ca817d71c5
Zakarya-Mks <zakaaryaaa@gmail.com>
2020-08-29 16:11:23 +0100

    fix: move src content to root folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: './js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -9,7 +9,7 @@ module.exports = {
   },
   mode: 'none',
   devServer: {
-    contentBase: path.join(__dirname, 'src'),
+    contentBase: path.join(__dirname, './'),
     port: 9000,
     open: true,
     hot: true,
